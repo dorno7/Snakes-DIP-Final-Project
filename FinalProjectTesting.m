0e18a51f44e2181821032cc6babd5384c3a3142f
dorno7 <48071432+dorno7@users.noreply.github.com>
2020-12-08 16:36:30 -0800

    Minor modifications for final turn in and testing

--- FinalProjectTesting.m
+++ FinalProjectTesting.m
@@ -10,24 +10,30 @@ Snaxel = {};
 % Select pixels to make up starting snake %
 figure
 imshow(Image)
+truesize([500 500])
 [x,y] = ginput;
 [height, width] = size(Image);
 % Put coordinates into a cell array
 for i=1:length(x)
     Snaxel(i) = {[x(i) y(i)]}; % Cell array containing coordinates
 end
+
 % Iterate calculating the new snaxels
 [MagImage, GradImage, ~, ~] = MagnitudeGradient(Image,0.6);
-for n=1:25
+for n=1:50
     if n == 1
         firstIter = Snaxel;
-    elseif n == 25
-        lastIter = Snaxel;
     end
     drawSnakewithLines(Snaxel, Image)
     title(['Iteration = ', num2str(n)])
     movegui('center')
     Snaxel = snakesDynamic(Snaxel, Image, MagImage, 0.5); % last param is alpha
+    
+    if n == 25
+        midIter = Snaxel;
+    elseif n == 50
+        lastIter = Snaxel;
+    end
 end
 
 % Show first and last images
@@ -35,6 +41,11 @@ close all
 figure
 drawSnakewithLines(firstIter, Image)
 title('Starting Image')
+movegui('center')
+
+figure
+drawSnakewithLines(midIter, Image)
+title('Mid Image')
 movegui('center')
 
 figure
